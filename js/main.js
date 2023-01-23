3c4f3edd6a113b18d3f5b2e982a66d1a9cd470ee
gon2410 <39386867+gon2410@users.noreply.github.com>
2023-01-23 15:34:49 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,32 +1,67 @@
 // listas
-const listadeMarcas = ["Renault", "Volkswagen", "Chevrolet"];
-
-const listadeRenault = ["Duster", "Sandero", "Captur"];
-const listadeVolkswagen = ["Vento", "Gol", "Golf"];
-const listadeChevrolet = ["S10", "Onix", "Corsa"];
-
 const listadeColores = ["Blanco Perla", "Negro", "Azul", "Rojo", "Gris", "Verde"];
-
 const listadeColoresAsientos = ["Negro", "Gris", "Verde", "Rojo"];
-
 const listaLlantas = ["Aleacion", "Chapa(Tazas)"]
 
 class Auto{
-    constructor(marca, modelo, color, colorAsientos, llantas) {
+    constructor(id, marca, modelo, color, colorAsientos, llantas, precio) {
+        this.id = id
         this.marca = marca;
         this.modelo = modelo;
         this.color = color;
         this.colorAsientos = colorAsientos;
         this.llantas = llantas;
+        this.precio = precio
     }
 
     display() {
-        return "Marca: " + this.marca + " - Modelo: " + this.modelo + " - Color: " + this.color + " - Color de Asientos: " + this.colorAsientos + " - Llantas: " + this.llantas;
+        return "\nMarca: " + this.marca + "\n - Modelo: " +
+                this.modelo + "\n - Color: " + this.color +
+                "\n - Color de Asientos: " + this.colorAsientos +
+                "\n - Llantas: " + this.llantas + "\n - Precio: $" +
+                this.precio;
+    }
+
+    verificar() {
+        if (this.llantas == "Aleacion") {
+            this.precio += 100_000;
+        }
     }
 }
 
+let renaultDuster = new Auto(0, "Renault", "Duster", "Negro", "Negro", "Chapa", 1_000_000);
+let renaultSandero = new Auto(1, "Renault", "Sandero", "Negro", "Negro", "Chapa", 950_000);
+let renaultCaptur = new Auto(2, "Renault", "Captur", "Negro", "Negro", "Chapa", 1_100_000);
+
+let volkswagenVento = new Auto(3, "Volkswagen", "Vento", "Negro", "Negro", "Chapa", 2_000_000);
+let volkswagenGol = new Auto(4, "Volkswagen", "Gol", "Negro", "Negro", "Chapa", 800_000);
+let volkswagenGolf = new Auto(5, "Volkswagen", "Golf", "Negro", "Negro", "Chapa", 1_250_000);
+
+let chevroletS10 = new Auto(6, "Chevrolet", "S10", "Negro", "Negro", "Chapa", 1_500_000);
+let chevroletOnix = new Auto(7, "Chevrolet", "Onix", "Negro", "Negro", "Chapa", 850_000);
+let chevroletCorsa = new Auto(8, "Chevrolet", "Corsa", "Negro", "Negro", "Chapa", 700_000);
+
+let arrayAutos = [renaultDuster, renaultSandero, renaultCaptur,
+                volkswagenVento, volkswagenGol, volkswagenGolf,
+                chevroletS10, chevroletOnix, chevroletCorsa];
 // objeto final
 let vehiculoFinal = new Auto();
+
+// eleccion inicial. Lista de autos.
+function elegirAuto(lista) {
+    let promptString = " ";
+    for (let i = 0; i < lista.length; i++) {
+        promptString += "\n" + i + " - " + lista[i].display() + "\n";
+    }
+
+    // validando ingreso
+    let eleccionUsuario = prompt(promptString + "\n Elegir un vehiculo:");
+    while ((eleccionUsuario.length == 0) || (!isNaN(eleccionUsuario) == false) || (parseInt(eleccionUsuario) < 0) || (parseInt(eleccionUsuario) > lista.length-1)) {
+        eleccionUsuario = prompt(promptString + "\n Respuesta invalida. Intente de nuevo:");
+    }
+
+    return lista[eleccionUsuario];
+}
 
 // toma una lista como parametro y el tipo (Marca, Modelo o Color) e imprime cada item
 function eleccionDeUsuario(lista, tipo) {
@@ -40,7 +75,7 @@ function eleccionDeUsuario(lista, tipo) {
 
     let eleccionUsuario = prompt(tipoListaString + "\n\n" + listaString + "\nEliga una opcion: ")
 
-    // validando que el ingreso del usuario sea un numero y que no sea mayor que el largo de la lista.
+    // validando ingreso
     while ((eleccionUsuario.length == 0) || (!isNaN(eleccionUsuario) == false) || (parseInt(eleccionUsuario) < 0) || (parseInt(eleccionUsuario) > lista.length-1)) {
         eleccionUsuario = prompt(tipoListaString + "\n\n" + listaString + "\nRespuesta invalida. Intente de nuevo: ")
     }
@@ -48,40 +83,20 @@ function eleccionDeUsuario(lista, tipo) {
     return lista[parseInt(eleccionUsuario)]
 }
 
-// eligiendo marca
-let marcaElegida = eleccionDeUsuario(listadeMarcas, "Marca");
-vehiculoFinal.marca = marcaElegida;
-
-
-// eligiendo modelo
-let modeloElegido;
-
-switch (marcaElegida) {
-    case "Renault":
-        modeloElegido = eleccionDeUsuario(listadeRenault, "Modelos")
-        break;
-    case "Volkswagen":
-        modeloElegido = eleccionDeUsuario(listadeVolkswagen, "Modelos")
-        break;
-    case "Chevrolet":
-        modeloElegido = eleccionDeUsuario(listadeChevrolet, "Modelos")
-        break;
-    default:
-        break;
-}
-
-vehiculoFinal.modelo = modeloElegido;
+// eligiendo auto
+let autoElegido = elegirAuto(arrayAutos);
 
 // eligiendo color
 let colorElegido = eleccionDeUsuario(listadeColores, "Colores");
-vehiculoFinal.color = colorElegido;
+autoElegido.color = colorElegido;
 
 // eligiendo color de asientos
 let colorAsientosElegido = eleccionDeUsuario(listadeColoresAsientos, "Colores de Asientos");
-vehiculoFinal.colorAsientos = colorAsientosElegido;
+autoElegido.colorAsientos = colorAsientosElegido;
 
 // eligiendo llantas
 let llantasElegidas = eleccionDeUsuario(listaLlantas, "Llantas");
-vehiculoFinal.llantas = llantasElegidas;
+autoElegido.llantas = llantasElegidas;
+autoElegido.verificar();
 
-alert("VEHICULO FINAL ELEGIDO: " + vehiculoFinal.display());
+alert("VEHICULO FINAL ELEGIDO: " + autoElegido.display());
