0adef9946a484e1810f7a8950827b1642cd6d134
gon2410 <goonolivera@hotmail.com>
2023-02-23 10:56:06 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
+let id = 0;
 class Auto{
-    constructor(id, marca, modelo, color, precio, coloresDisponibles, motores, motor) {
+    constructor(id, marca, modelo, color, precio, coloresDisponibles, motores, transmisiones, motor, transmision) {
         this.id = id
         this.marca = marca;
         this.modelo = modelo;
@@ -29,23 +30,24 @@ let gris_oscuro = {color: "Gris_Oscuro", hex: "#333333"};
 let rojo = {color: "Rojo", hex: "#823839"};
 
 // creando autos
-let renaultDuster = new Auto(0, "Renault", "Duster", "Negro", 1_000_000, [azul, beige, blanco, gris], [{motor: "1.6", hp: 115, torque: 150}, {motor: "1.3T", hp: 155, torque: 250}]);
-let renaultSandero = new Auto(1, "Renault", "Sandero", "Negro", 950_000, [azul, blanco, gris, gris_oscuro, rojo], [{motor: "1.6", hp: 115, torque: 150}]);
-let renaultCaptur = new Auto(2, "Renault", "Captur", "Negro", 1_100_000, [blanco, gris, gris_oscuro, rojo], [{motor: "1.6", hp: 115, torque: 156},{motor: "2.0", hp: 143, torque: 193}]);
+let renaultDuster = new Auto(null, "Renault", "Duster", "Negro", 1_500_000, [azul, beige, blanco, gris], [{motor: "1.6", hp: 115, torque: 150}, {motor: "1.3T", hp: 155, torque: 250}]);
+let renaultSandero = new Auto(null, "Renault", "Sandero", "Negro", 1_150_000, [azul, blanco, gris, gris_oscuro, rojo], [{motor: "1.6", hp: 115, torque: 150}]);
+let renaultCaptur = new Auto(null, "Renault", "Captur", "Negro", 1_250_000, [blanco, gris, gris_oscuro, rojo], [{motor: "1.6", hp: 115, torque: 156},{motor: "2.0", hp: 143, torque: 193}]);
 
-let volkswagenVirtus = new Auto(3, "Volkswagen", "Virtus", "Negro", 2_000_000, [azul, blanco, gris, gris_oscuro, negro], [{motor: "1.6", hp: 110, torque: 155}, {motor: "1.4T", hp: 150, torque: 250}]);
-let volkswagenPolo = new Auto(4, "Volkswagen", "Polo", "Negro", 800_000, [azul, blanco, gris, gris_oscuro, negro, rojo], [{motor: "1.6", hp: 110, torque: 155}, {motor: "1.4T", hp: 150, torque: 250}]);
-let volkswagenAmarok = new Auto(5, "Volkswagen", "Amarok", "Negro", 1_250_000, [azul, beige, blanco, gris, gris_oscuro, negro], [{motor: "2.0", hp: 140, torque: 340}, {motor: "2.0", hp: 180, torque: 420}, {motor: "3.0 V6", hp: 260, torque: 580}]);
+let volkswagenVirtus = new Auto(null,"Volkswagen", "Virtus", "Negro", 2_000_000, [azul, blanco, gris, gris_oscuro, negro], [{motor: "1.6", hp: 110, torque: 155}, {motor: "1.4T", hp: 150, torque: 250}]);
+let volkswagenPolo = new Auto(null,"Volkswagen", "Polo", "Negro", 1_500_000, [azul, blanco, gris, gris_oscuro, negro, rojo], [{motor: "1.6", hp: 110, torque: 155}, {motor: "1.4T", hp: 150, torque: 250}]);
+let volkswagenAmarok = new Auto(null,"Volkswagen", "Amarok", "Negro", 3_250_000, [azul, beige, blanco, gris, gris_oscuro, negro], [{motor: "2.0", hp: 140, torque: 340}, {motor: "2.0", hp: 180, torque: 420}, {motor: "3.0 V6", hp: 260, torque: 580}]);
 
-let chevroletS10 = new Auto(6, "Chevrolet", "S10", "Negro", 1_500_000, [azul, blanco, gris, negro,], [{motor: "2.8", hp: 200, torque: 500}]);
-let chevroletOnix = new Auto(7, "Chevrolet", "Onix", "Negro", 850_000, [blanco, negro, rojo], [{motor: "1.0", hp: 116, torque: 160}]);
-let chevroletCruze = new Auto(8, "Chevrolet", "Cruze", "Negro", 700_000, [azul, blanco, gris, negro], [{motor: "1.4", hp: 153, torque: 245}]);
+let chevroletS10 = new Auto(null,"Chevrolet", "S10", "Negro", 2_500_000, [azul, blanco, gris, negro,], [{motor: "2.8", hp: 200, torque: 500}]);
+let chevroletOnix = new Auto(null,"Chevrolet", "Onix", "Negro", 1_050_000, [blanco, negro, rojo], [{motor: "1.0", hp: 116, torque: 160}]);
+let chevroletCruze = new Auto(null,"Chevrolet", "Cruze", "Negro", 1_700_000, [azul, blanco, gris, negro], [{motor: "1.4", hp: 153, torque: 245}]);
 
 
 // agrego los autos creados a la lista
 let arrayAutos = [renaultDuster, renaultSandero, renaultCaptur,
                 volkswagenVirtus, volkswagenPolo, volkswagenAmarok,
                 chevroletS10, chevroletOnix, chevroletCruze];
+let carrito = [];
 
 // guardo autos en sessionStorage
 for (let i = 0; i < arrayAutos.length; i++) {
@@ -56,41 +58,46 @@ for (let i = 0; i < arrayAutos.length; i++) {
 
 // creo el objeto Auto final (el que elige el usuario)
 let autoFinal = new Auto();
-
-
 let imagenDiv = document.querySelector("#imagen");
 let infoDiv = document.querySelector("#texto");
 let btnComprar = document.querySelector("#botonComprar");
-
-deshacer();
+const dropdownCarrito = document.querySelector("#dropdownCarrito");
 
 let divAutos = document.querySelector("#div_autos")
 let select = document.createElement("select");
 select.className = "form-select";
 
-// creo el select de opciones obteniendo los datos del sessionStorage
-for (let i = 0; i < arrayAutos.length; i++) {
-    let option = document.createElement("option");
-    let autoJson = sessionStorage.getItem(arrayAutos[i].modelo);
-    let auto = JSON.parse(autoJson);
 
-    option.innerHTML = auto.marca + " " + auto.modelo;
-    option.value = auto.modelo;
-    select.append(option);
+// creo el select de opciones obteniendo los datos del sessionStorage
+function crearOpciones() {
+    deshacer();
+    for (let i = 0; i < arrayAutos.length; i++) {
+        let option = document.createElement("option");
+        let autoJson = sessionStorage.getItem(arrayAutos[i].modelo);
+        let auto = JSON.parse(autoJson);
+    
+        option.innerHTML = auto.marca + " " + auto.modelo;
+        option.value = auto.modelo;
+        select.append(option);
+    }
+    
+    // llamo al sessionStorage y le pido el objeto con la key elegida en el select y llamo a la funcion autoElegido()
+    select.addEventListener("change", () => {
+        let autoJson = sessionStorage.getItem(select.value);   
+        let auto = JSON.parse(autoJson);
+        autoElegido(auto); 
+    })
+    
+    divAutos.append(select);
 }
 
-// llamo al sessionStorage y le pido el objeto con la key elegida en el select y llamo a la funcion autoElegido()
-select.addEventListener("change", () => {
-    let autoJson = sessionStorage.getItem(select.value);   
-    let auto = JSON.parse(autoJson);
-    autoElegido(auto); 
-})
 
-divAutos.append(select);
 
 // renderizo imagen inicial e informacion del auto
 function autoElegido(auto) {
 
+    autoFinal.id = id
+    id += 1;
     autoFinal.marca = auto.marca;
     autoFinal.modelo = auto.modelo;
     autoFinal.precio = auto.precio;
@@ -107,6 +114,7 @@ function autoElegido(auto) {
     modeloAutoElegido.innerHTML = auto.marca + " " + auto.modelo;
 
     imagen.src = "./media/" + auto.marca + "_" + auto.modelo + "_Blanco.png";
+    imagen.className = "img-fluid";
     imagen.width = 500;
 
     imagenDiv.append(imagen);
@@ -193,13 +201,76 @@ function eleccionDeColor(auto, imagen) {
 
 // boton comprar
 btnComprar.addEventListener("click", () => {
-        swal({
-        title: "Felicitaciones!",
-        text: "Has adquirido el suiguiente vehiculo!" + autoFinal.display(),
-        icon: "success",
-        button: "OK",
-    });
+    carrito.push(autoFinal)
+    deshacer();
+    autoFinal = new Auto()
+    btnComprar.setAttribute("disabled", "");
+    actualizarCarrito()
+    Toastify({
+        text: "Agregado al carrito",
+        duration: 3000,
+        gravity: "top",
+        position: "right",
+        style: {
+            background: "#3B71CA"
+        }
+    }).showToast()
 })
+
+function actualizarCarrito() {
+    dropdownCarrito.innerHTML = "";
+    let count = 0;
+    for (let i = 0; i < carrito.length; i++) {
+        count += carrito[i].precio;
+        const image = document.createElement("img");
+        image.style.width = "4rem";
+        image.src = "./media/" + carrito[i].marca + '_' + carrito[i].modelo + "_" + carrito[i].color + '.png';
+
+        const dropdownItem = document.createElement("li");
+        dropdownItem.className = "dropdown-item";
+
+        dropdownItem.append(image)
+        dropdownItem.innerHTML += carrito[i].marca + " " + carrito[i].modelo;
+
+        const itemButton = document.createElement("button");
+        itemButton.className = "btn btn-sm btn-link";
+        itemButton.innerHTML = `<i class="fa fa-close"></i>`;
+
+        
+        // console.log(carrito[i].id);
+        itemButton.addEventListener("click", () => {
+
+            for (let j = 0; j < carrito.length; j++) {
+                if (carrito[i].id == carrito[j].id) {
+                    carrito.splice(i, 1);
+                    // console.log(carrito)
+                    actualizarCarrito();
+                    break;
+                }
+            }
+            
+        })
+
+        dropdownItem.append(itemButton);
+        dropdownCarrito.append(dropdownItem);
+        
+    }
+    const buttonComprar = document.createElement("button");
+    buttonComprar.className = "btn btn-sm btn-light m-1";
+    buttonComprar.innerHTML = "Finalizar Compra";
+
+    buttonComprar.addEventListener("click", () => {
+        swal({
+            title: "Felicitaciones!",
+            text: "Monto total: $" + count,
+            icon: "success",
+            button: "Pagar",
+        });
+        // carrito = []
+        // actualizarCarrito();
+    })
+    dropdownCarrito.append(buttonComprar);
+}
 
 // funcion deshacer inicial para que no quede en blanco la pagina
 function deshacer() {
@@ -220,3 +291,6 @@ function deshacer() {
     imagenDiv.append(imagen);
     infoDiv.append(imagenInfoCargando);
 }
+
+
+crearOpciones();
